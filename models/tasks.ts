935ad9a20d1645ef27e72ad2fd8871f5e00c0a2b
Alysha Dosdos <alyshadosdos@gmail.com>
2024-12-03 19:13:25 +1300

    create db functions and model

---
+++ models/tasks.ts
@@ -0,0 +1,6 @@
+export interface Task {
+  id: number
+  name: string
+  details: string
+  completed: boolean
+}
---
+++ server/db/db.ts
@@ -0,0 +1,42 @@
+import connection from './connection'
+import { Task } from '../../models/tasks'
+
+const db = connection
+
+export async function getAllTasks(): Promise<Task[]> {
+  const tasks = await db('tasks').select('*')
+  return tasks
+}
+
+export async function getTaskById(id: number): Promise<Task> {
+  const task = await db('tasks')
+    .select('id', 'name', 'details', 'completed')
+    .where({ id })
+    .first()
+  return task as Task
+}
+
+//add function
+export async function addNewTask(task: Task): Promise<Task> {
+  const { id, name, details, completed } = task
+  const newTask = {
+    id,
+    name,
+    details,
+    completed,
+  }
+  return await db('events').insert(newTask)
+}
+
+//update function
+export async function updateTask(updatedTask: Task): Promise<void> {
+  const { id, name, details, completed } = updatedTask
+  return await db('locations')
+    .where({ id })
+    .update({ name, details, completed })
+}
+
+//delete function
+export async function deleteTask(id: number): Promise<void> {
+  await db('tasks').where({ id }).del()
+}
